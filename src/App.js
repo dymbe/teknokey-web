8cc7d22fb44b58b0251332ea6de4e13b813e1127
Erik Dymbe <erik.dymbe@gmail.com>
2019-04-27 00:53:57 +0200

    Add lint + lint whole project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import FacebookLogin from 'react-facebook-login'
+import config from './config'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <h1>Teknokey</h1>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Learn React
+              Learn React
           </a>
+          <FacebookLogin
+            appId={config.fbAppId}
+            autoLoad
+            fields='name,email,picture'
+            onClick={(e) => console.log(e)}
+            callback={(e) => console.log(e)} />
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+  fbAppId: 259182938010731
+}
+
+export default config
